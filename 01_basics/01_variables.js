cf16572798dd9888e286ce0262576446a53c6cdb
sakshi sharma <122162031+sakshisharma78@users.noreply.github.com>
2024-02-06 17:18:05 +0000

    main

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 124455;
+var accountPassword = "123";
+let accountemail = "sakshi@gmail.com";
+aacountcity="jaipur"
+
+// accountId = 2 not allowed
+
+accountCity = "rajasthan"
+accountemail = "sakshis.@gmail.com"
+accountPassword ="123456"
+console.log(accountId);
+
+console.table([accountCity , accountemail, accountPassword ,])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"//way to use code as newer state
+
+let name="sakshi"
+let no=3
+let islogged=false
+
+
+//number =2 to power 53
+//bigint
+//string =""
+//boolean=true/false
+//null =standalone value
+//underdefined =>
+
+//symbol=unique
+
+console.log(typeof no)
